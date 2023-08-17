d69b8f8d03b571e036fc3d7c04867a271ae10536
Manuel Ortiz <manuel.ortiz796@gmail.com>
2023-08-17 15:40:01 -0300

    Se agrego funcionalidad de agregar tarea
    Se creo objeto y funcion constructora para luego meterlo en un array y mostrar elementos que decriben la tarea a recordar.

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@ let cargarTarea = document.getElementById('cargar-tarea')
 let listadoTareas = document.getElementById("listado-tareas")
 let ingresarTarea = document.getElementById("ingresarTarea")
 let img1 = document.getElementById("img1")
+let seleccionarImg = document.getElementById('touch2')
 let contenedorIconos = document.getElementById("contenedorIconos")
 let cantidadDias = document.getElementById("cantidadDias")
 
+let tarea = []
 
 
 btn1.addEventListener('click', () => {
@@ -16,19 +18,45 @@ btn1.addEventListener('click', () => {
     cargarTarea.style.display = 'flex'
 })
 
+
 btn2.addEventListener('click', () => {
+    //desaparece pantalla 2
     cargarTarea.style.display = 'none'
+    //aparece pantalla 3
     listadoTareas.style.display = 'flex'
     //guardo el valor del input en variable
     let tareaIngresada = ingresarTarea.value
     //guardo la cantiadad de dias en variable
     let venceEn = cantidadDias.value
     //a la tercer pantalla le agrego un div con un h3, un P y un button
-    listadoTareas.innerHTML += `<div class="container-tarea"> <h3>` + tareaIngresada + `</h3> <p class="tiempo-restante"> RECORDAR EN ` + venceEn + ` DIAS</p> 
+    
+    //creo objeto
+    function Objeto(imagen,  titulo, dias){
+        this.imagen = imagen
+        this.titulo = titulo
+        this.dias = dias
+    }
+
+    //funcion constructora
+    function agregarObjeto(imagen, titulo, dias){
+        let elementosCard = new Objeto(imagen, titulo, dias)
+        tarea.push(elementosCard)
+    }
+
+    agregarObjeto("imagen", tareaIngresada, venceEn)
+
+    tarea.map((tarea)=>{
+        listadoTareas.innerHTML += `<div class="container-tarea"> <h3>` + tareaIngresada + `</h3> <p class="tiempo-restante"> Recordar en ` + venceEn + ` dias</p> 
     <button class="tareaHecha">Hecho </button> </div>`
+    })
 })
 
+touch2.addEventListener('click', ()=>{
+
+})
+
+//al hacer click en boton agregar de pantalla 3
 btn3.addEventListener('click', () => {
-    cargarTarea.style.display = 'flex'
     listadoTareas.style.display = 'none'
+    cargarTarea.style.display = 'flex'
 })
